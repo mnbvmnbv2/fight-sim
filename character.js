4aa806620f9034989c30b36b0a38597ba494ec31
mnbvmnbv2 <ferdinand@ytteborg.com>
2022-02-11 18:21:17 +0100

    testing optional constructor

--- character.js
+++ character.js
@@ -1,5 +1,11 @@
 class Character {
-	constructor() {
-		this.health = 100;
+	constructor({name : undefined, health : 100} = {name : undefined, health : 100}) {
+		this.health = health;
+		this.name = name;
 	}
+	constructor({a = 'default a value', b = 'default b value', c = 'default c value'} = {a:'default option a', b:'default option b', c:'default option c'}) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	  }
 }
--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 let a = new Character();
 console.log(a);
+
+let b = new Character('Sam');
+console.log(b);
+
+let c = new Character('Brian', 50);
+console.log(c);
