9bdecff5254134240ff5229b366e5e80da8a1353
mnbvmnbv2 <ferdinand@ytteborg.com>
2022-03-10 18:04:52 +0100

    Fix default values

--- character.js
+++ character.js
@@ -1,11 +1,6 @@
 class Character {
-	constructor({name : undefined, health : 100} = {name : undefined, health : 100}) {
+	constructor(health = 100, name = null) {
 		this.health = health;
 		this.name = name;
 	}
-	constructor({a = 'default a value', b = 'default b value', c = 'default c value'} = {a:'default option a', b:'default option b', c:'default option c'}) {
-		this.a = a;
-		this.b = b;
-		this.c = c;
-	  }
 }
--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 let a = new Character();
 console.log(a);
 
-let b = new Character('Sam');
+let b = new Character(50, 'Sam');
 console.log(b);
 
-let c = new Character('Brian', 50);
+let c = new Character(550);
 console.log(c);
+
+let d = new Character(undefined, 'Pam');
+console.log(d);
